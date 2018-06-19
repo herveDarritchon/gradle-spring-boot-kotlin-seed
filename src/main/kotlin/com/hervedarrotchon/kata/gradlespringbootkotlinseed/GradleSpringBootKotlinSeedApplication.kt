1a2a6f1140e298fd68eb889ab086c3fa94d8fc34
Hervé Darritchon <herve.darritchon@gmail.com>
2018-06-19 07:07:59 +0200

    Initial commit from Spring Initializer.

---
+++ src/main/kotlin/com/hervedarrotchon/kata/gradlespringbootkotlinseed/GradleSpringBootKotlinSeedApplication.kt
@@ -0,0 +1,11 @@
+package com.hervedarrotchon.kata.gradlespringbootkotlinseed
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GradleSpringBootKotlinSeedApplication
+
+fun main(args: Array<String>) {
+    runApplication<GradleSpringBootKotlinSeedApplication>(*args)
+}
